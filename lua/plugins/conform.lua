760b991626df7fef04c07e4639a01f4581da612e
Oliver Brewka <oliver.brewka@9elements.com>
2026-01-24 14:42:24 +0100

    Replace formatter.nvim with conform.nvim

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,38 @@
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+
+  -- This will provide type hinting with LuaLS
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      c = { 'clangformat' },
+      cpp = { 'clangformat' },
+      go = { 'gofmt' },
+      json = { 'clangformat' },
+      lua = { 'stylua' },
+      python = { 'black' },
+      yaml = { 'prettier' },
+      ['_'] = { 'trim_whitespace', lsp_format = 'prefer' },
+    },
+    -- Set default options
+    default_format_opts = {
+      lsp_format = 'fallback',
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500 },
+    -- Customize formatters
+    formatters = {
+      shfmt = {
+        append_args = { '-i', '2' },
+      },
+    },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    -- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
